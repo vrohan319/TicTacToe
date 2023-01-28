43a6c801f13f891e1fb720c19a19ed781315c402
Aman Kumar <vrohan319@gmail.com>
2023-01-29 00:15:58 +0530

    validate the input in form

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 const overlayCancelBtn = document.getElementById("overlayCancelBtn");
 const formElement = document.querySelector("form");
+const error = document.getElementById("error");
+const formControl = document.getElementById("formControl");
 
 const playerConfigOverlay = document.getElementById("config-overlay");
 const backdrop = document.getElementById("backdrop");
--- scripts/config.js
+++ scripts/config.js
@@ -4,13 +4,20 @@ function openPlayerConfig() {
 }
 
 function closeOverlay() {
+  formControl.classList.remove("error");
+  error.textContent = "";
   playerConfigOverlay.style.display = "none";
   backdrop.style.display = "none";
 }
 
 function savePlayerConfig(event) {
-  event.preventDefault(); 
-  const formData = new FormData(event.target) ;
-  const enteredPlayerName = formData.get("playerName") ;
-  console.log(enteredPlayerName) ;
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playerName").trim();
+  //   console.log(enteredPlayerName) ;
+  if (!enteredPlayerName) {
+    formControl.classList.add("error");
+    error.textContent = "Please enter a valid value";
+    return ;
+  }
 }
